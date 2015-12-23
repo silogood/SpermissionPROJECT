ea485c62c14e728bf1c8ef2e0bdc2ce1f8941270
Jayparkjiho <159wlgh@naver.com>
2015-12-23 19:17:16 +0900

    lv

--- app/src/main/java/com/silogood/s_permissions/MainActivity.java
+++ app/src/main/java/com/silogood/s_permissions/MainActivity.java
@@ -52,4 +52,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return true;
     }   //메뉴
 //
+
+   /* @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }    //옵션 이여
+
+        return super.onOptionsItemSelected(item);
+    }*/
 }
